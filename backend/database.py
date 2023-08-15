1433dbe07f5dac7cab72c8960736b8175d574ec3
Hassan-Memon <hassan.ghaddai@live.com>
2023-08-15 21:48:10 +0500

    Harper database setup compeleted

---
+++ backend/database.py
@@ -0,0 +1,12 @@
+import harperdb
+
+HARPERDB_PASSWORD = "0321237967a1A!"
+HARPERDB_URL= "https://todo-todo.harperdbcloud.com"
+HARPERDB_USERNAME = "HassanMemon"
+
+db = harperdb.HarperDB(
+    url = HARPERDB_URL,
+    username = HARPERDB_USERNAME,
+    password = HARPERDB_PASSWORD
+)
+
